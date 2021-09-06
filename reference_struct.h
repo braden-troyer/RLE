cb7bc1d4e72e5f446026da90b63f7f17a2a85c48
Braden Troyer <braden.troyer04@gmail.com>
2021-09-06 11:56:57 -0500

    Add reference_struct.h

---
+++ reference_struct.h
@@ -0,0 +1,11 @@
+
+typedef struct Reference_List {
+    char source_type;
+    int author_num;
+    char *author_name;
+    int year;
+    char *title;
+    char *edition;
+    char *place_of_publication;
+    char *publisher;
+}RList;
